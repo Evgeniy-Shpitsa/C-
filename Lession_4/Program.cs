2fb013f47bc1937c7907fc0579a85c503caaeee6
Evgeniy <evgeniy.shpitsa@gmail.com>
2022-08-06 20:02:23 +0300

    DZ

---
+++ Lession_4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+int sum = a;
+for (int i = 1; i<b; i++)
+    {
+        sum = sum * a;
+    }
+Console.WriteLine(sum);*/
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число ");
+int x = Convert.ToInt32(Console.ReadLine());
+int size = x.ToString().Length;
+int sum = 0;
+for (int i = 0; i < size; i++)
+    {
+    sum = sum + (x % 10);
+    x = x / 10;
+    }
+Console.WriteLine(sum);
