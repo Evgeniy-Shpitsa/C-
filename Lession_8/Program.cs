859cb9432183a38d6464f411170f57bd2bb40fe4
Evgeniy <evgeniy.shpitsa@gmail.com>
2022-08-21 20:47:41 +0300

    DZ

---
+++ Lession_8/Program.cs
@@ -0,0 +1,196 @@
+﻿/*//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Console.Write("Введите кол-во строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int[m,n];
+
+static void PrintArray(int[,] matr)
+    {
+      for (int i = 0; i < matr.GetLength(0); i++)
+      {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+      }
+    }
+static void FillArray(int[,] matr)
+    {
+      for (int i = 0; i < matr.GetLength(0); i++)
+      {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(0, 10);
+        }
+      }
+    }
+void descending(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+FillArray(array);
+PrintArray(array);
+Console.Write("По убыванию - ");
+descending(array);
+PrintArray(array);
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Console.Write("Введите кол-во строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int[m,n];
+static void FillArray(int[,] matr)
+    {
+      for (int i = 0; i < matr.GetLength(0); i++)
+      {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(0, 10);
+        }
+      }
+    }
+static void PrintArray(int[,] matr)
+    {
+      for (int i = 0; i < matr.GetLength(0); i++)
+      {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+      }
+    }
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+FillArray(array);
+PrintArray(array);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) ");
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Console.Write("Введите число строк 1-й матрицы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число столбцов 2-й матрицы: ");
+int p = Convert.ToInt32(Console.ReadLine());
+static void PrintArray(int[,] matr)
+    {
+      for (int i = 0; i < matr.GetLength(0); i++)
+      {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+      }
+    }
+static void FillArray(int[,] matr)
+    {
+      for (int i = 0; i < matr.GetLength(0); i++)
+      {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(0, 10);
+        }
+      }
+    }
+int[,] firstMartrix = new int[m, n];
+FillArray(firstMartrix);
+Console.WriteLine($"\nПервая матрица:");
+PrintArray(firstMartrix);
+int[,] secomdMartrix = new int[n, p];
+FillArray(secomdMartrix);
+Console.WriteLine($"\nВторая матрица:");
+PrintArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+        resultMatrix[i,j] = firstMartrix[i,j] * secomdMartrix[i,j];
+    }
+  }
+}
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"Произведение первой и второй матриц:");
+PrintArray(resultMatrix);
+
+// Задача 62. Заполните спирально массив 4 на 4.
+int n = 4;
+int[,] sqareMatrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+{
+  sqareMatrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+WriteArray(sqareMatrix);
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}*/
