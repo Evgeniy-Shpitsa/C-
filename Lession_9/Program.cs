ada9f26324eea0ae43ee75790e6645bd247701c2
Evgeniy <evgeniy.shpitsa@gmail.com>
2022-08-25 21:28:02 +0300

    DZ

---
+++ Lession_9/Program.cs
@@ -0,0 +1,43 @@
+﻿/*// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void PrintNumber(int m, int n)
+{
+  if (n > m) return;
+  PrintNumber(m, n + 1);
+  Console.Write(n + ", ");
+}
+PrintNumber(m, n);
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int temp = m;
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов равна {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+PrintSumm(m, n, temp=0);
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+Console.Write(Ack(m, n));*/
